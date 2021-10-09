ef2bf7bea67ac044c385e1c5b07b0a35602108f9
john7461 <a.dagne16@gmail.com>
2021-10-09 10:49:26 -0700

    New home page + sample page

--- New Website/assets/js/main.js
+++ New Website/assets/js/main.js
@@ -106,7 +106,7 @@
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
-
+  
   /**
    * Mobile nav dropdowns activate
    */
@@ -302,4 +302,62 @@ function open8() {
 }
 function open9() {
   window.open('https://www.digipen.edu/', '_blank');
+}
+function open10() {
+  window.open('https://www.amazon.com', '_blank');
+}
+
+const tiltEffectSettings = {
+  max: 25, // max tilt rotation (degrees (deg))
+  perspective: 1000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
+  scale: 1.1, // transform scale - 2 = 200%, 1.5 = 150%, etc..
+  speed: 500, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
+  easing: "cubic-bezier(.03,.98,.52,.99)" // easing (transition-timing-function) of the enter/exit transition
+};
+
+const cards = document.querySelectorAll(".card");
+cards.forEach(card => {
+  card.addEventListener("mouseenter", cardMouseEnter);
+  card.addEventListener("click", openPage);
+  card.addEventListener("mousemove", cardMouseMove);
+  card.addEventListener("mouseleave", cardMouseLeave);
+});
+
+function cardMouseEnter(event) {
+  setTransition(event);
+}
+function openPage(){
+  window.open('/engage.html', '_self');
+}
+function cardMouseMove(event) {
+  const card = event.currentTarget;
+  const cardWidth = card.offsetWidth;
+  const cardHeight = card.offsetHeight;
+  const centerX = card.offsetLeft + cardWidth/2;
+  const centerY = card.offsetTop + cardHeight/2;
+  const mouseX = event.clientX - centerX;
+  const mouseY = event.clientY - centerY;
+  const rotateXUncapped = (+1)*tiltEffectSettings.max*mouseY/(cardHeight/2);
+  const rotateYUncapped = (-1)*tiltEffectSettings.max*mouseX/(cardWidth/2);
+  const rotateX = rotateXUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
+                  (rotateXUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateXUncapped);
+  const rotateY = rotateYUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
+                  (rotateYUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateYUncapped);
+
+  card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) 
+                          scale3d(${tiltEffectSettings.scale}, ${tiltEffectSettings.scale}, ${tiltEffectSettings.scale})`;
+}
+
+function cardMouseLeave(event) {
+  event.currentTarget.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`;
+  setTransition(event);
+}
+
+function setTransition(event) {
+  const card = event.currentTarget;
+  clearTimeout(card.transitionTimeoutId);
+  card.style.transition = `transform ${tiltEffectSettings.speed}ms ${tiltEffectSettings.easing}`;
+  card.transitionTimeoutId = setTimeout(() => {
+    card.style.transition = "";
+  }, tiltEffectSettings.speed);
 }
