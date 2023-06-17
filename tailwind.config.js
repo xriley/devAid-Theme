a44f8fcf0362a482beb87354d9594e9319f6d541
Sidharth R <rsidharth53@gmail.com>
2023-06-17 16:27:37 -0700

    changed layout for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["build\\space.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mohave': ["Mohave", 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
